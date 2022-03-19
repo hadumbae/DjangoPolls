c2759ff363c5bab32f651714b9c5a15cb9da7441
August Lin <cmcdnn6919@gmail.com>
2022-03-19 19:12:25 +0630

    19.03.2022 Update

---
+++ pollster/polls/admin.py
@@ -0,0 +1,29 @@
+from dataclasses import field
+from pyexpat import model
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Question, Choice
+
+# admin.site.register(Choice)
+# admin.site.register(Question)
+
+
+admin.site.site_header = "Polls Admin"
+admin.site.site_title = "Pollster | Polls"
+admin.site.index_title = "Welcome to the Pollster Admin area."
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+                 ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}), ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
---
+++ pollster/steps.md
@@ -0,0 +1,8 @@
+1. Create App
+2. Connect App in Admin
+3. Create Models and Migrations in App
+4. Setup Database Driver in Settings and Migrate
+5. Create URL files and connect in Admin
+6. Setup Templates Directory in Settings
+7. Create Base Layout Templates
+8. Extend Layouts to create new Templates
